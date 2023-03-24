e24d64f5870df454fdb08c42bdddbc87ff12893a
Felipe Bueno <felipebueno2201@gmail.com>
2023-03-24 03:46:18 -0300

    courses tables

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -3,13 +3,15 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
 export async function usersPost(req: Request, res: Response) {
-  const { email, password, image, username } = req.body;
+  const { email, password, username, image } = req.body;
 
   try {
-    const user = await userService.createUser({ email, password, image, username });
+    const user = await userService.createUser({ email, password, username, image });
     return res.status(httpStatus.CREATED).json({
       id: user.id,
       email: user.email,
+      username: user.username,
+      image: user.image,
     });
   } catch (error) {
     if (error.name === 'DuplicatedEmailError') {
--- src/services/authentication-service/index.ts
+++ src/services/authentication-service/index.ts
@@ -1,10 +1,10 @@
-import sessionRepository from "@/repositories/session-repository";
-import userRepository from "@/repositories/user-repository";
-import { exclude } from "@/utils/prisma-utils";
-import { User } from "@prisma/client";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { invalidCredentialsError } from "./errors";
+import sessionRepository from '@/repositories/session-repository';
+import userRepository from '@/repositories/user-repository';
+import { exclude } from '@/utils/prisma-utils';
+import { User } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { invalidCredentialsError } from './errors';
 
 async function signIn(params: SignInParams): Promise<SignInResult> {
   const { email, password } = params;
@@ -16,13 +16,19 @@ async function signIn(params: SignInParams): Promise<SignInResult> {
   const token = await createSession(user.id);
 
   return {
-    user: exclude(user, "password"),
+    user: exclude(user, 'password'),
     token,
   };
 }
 
 async function getUserOrFail(email: string): Promise<GetUserOrFailResult> {
-  const user = await userRepository.findByEmail(email, { id: true, email: true, password: true });
+  const user = await userRepository.findByEmail(email, {
+    id: true,
+    email: true,
+    password: true,
+    image: true,
+    username: true,
+  });
   if (!user) throw invalidCredentialsError();
 
   return user;
@@ -43,18 +49,18 @@ async function validatePasswordOrFail(password: string, userPassword: string) {
   if (!isPasswordValid) throw invalidCredentialsError();
 }
 
-export type SignInParams = Pick<User, "email" | "password">;
+export type SignInParams = Pick<User, 'email' | 'password'>;
 
 type SignInResult = {
-  user: Pick<User, "id" | "email">;
+  user: Pick<User, 'id' | 'email'>;
   token: string;
 };
 
-type GetUserOrFailResult = Pick<User, "id" | "email" | "password">;
+type GetUserOrFailResult = Pick<User, 'id' | 'email' | 'password'>;
 
 const authenticationService = {
   signIn,
 };
 
 export default authenticationService;
-export * from "./errors";
+export * from './errors';
